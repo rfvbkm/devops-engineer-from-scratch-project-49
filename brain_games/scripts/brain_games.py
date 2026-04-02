6bcb7f5c3cd93d7a197a2a003fe889488fb58ccb
Kamil Kayumov <kamil.kayumov@diginetica.com>
2026-04-02 23:42:11 +0300

    finalize step 3

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -2,12 +2,14 @@ import prompt
 
 
 def main():
-    print('Welcome to the Brain Games!')
+    print("Welcome to the Brain Games!")
     welcome_user()
 
-def welcome_user():
-    name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}!')
 
-if __name__ == '__main__':
+def welcome_user():
+    name = prompt.string("May I have your name? ")
+    print(f"Hello, {name}!")
+
+
+if __name__ == "__main__":
     main()
